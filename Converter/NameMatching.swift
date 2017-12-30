0d44eb6b7a3a6682457116adaacb6eb8deb34483
Wesley Wickwire <wickwirew@gmail.com>
2017-12-30 16:55:10 -0500

    added ability for loose naming scheme

---
+++ Converter/NameMatching.swift
@@ -0,0 +1,17 @@
+//
+//  NameMatching.swift
+//  Converter
+//
+//  Created by Wes Wickwire on 12/30/17.
+//  Copyright © 2017 weswickwire. All rights reserved.
+//
+
+import Foundation
+
+
+public enum NameMatching {
+    case loose
+    case strict
+}
+
+
